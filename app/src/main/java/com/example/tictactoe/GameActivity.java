a4fc003bd625f9187121048bd02450702e14b45b
Greg Pope <gapope1@gmail.com>
2020-02-23 17:02:31 -0500

    Buttons work

--- app/src/main/java/com/example/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/tictactoe/GameActivity.java
@@ -1,6 +1,9 @@
 package com.example.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,20 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button start = findViewById(R.id.quitButton);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent actIntent = new Intent(v.getContext(), MenuActivity.class);
+                startActivityForResult(actIntent, 0);
+            }
+        });
     }
+
+    /*
+    private void checkWin() {
+
+    }
+    */
 }
